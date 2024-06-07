19c475af88d9fee1d78f422e6c1b9d8ac024ebea
Godwin <godwinchuks032@gmail.com>
2024-06-07 04:00:12 +0100

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Resolve the lockbox"""
+
+def canUnlockAll(boxes):
+    """Check if all boxes can be opened.
+    Args:
+        boxes (list): List of lists, where each sublist contains keys.
+    Returns:
+        bool: True if all boxes can be opened, otherwise False.
+    """
+    opened = [False] * len(boxes)
+    opened[0] = True
+    keys = set(boxes[0])
+
+    while keys:
+        new_keys = keys.pop()
+        if new_keys < len(boxes) and not opened[new_keys]:
+            opened[new_keys] = True
+            keys.update(boxes[new_keys])
+
+    return all(opened)
