7e280cd7a060faf5f3d136edde73cb15b269dc6e
Godwin <godwinchuks032@gmail.com>
2024-06-21 04:25:46 +0100

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,27 @@
 import sys
 import signal
 
+
+def print_metrics(status_code_count, total_file_size):
+    """Print the collected metrics
+        Processing and Storing Metrics
+        track the total number file
+        track the total number occurance of each status code
+    Args:
+        status_code_count: dict of the status of the code
+        total_file_size: total size of the file
+    Retunr:
+        0
+    """
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(status_code_count.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
 total_file_size = 0
+code = 0
+line_count = 0
 status_code_count = {
     '200': 0,
     '301': 0,
@@ -15,32 +35,24 @@ status_code_count = {
     '405': 0,
     '500': 0
 }
-line_count = 0
 
-def print_metrics():
-    """Print the collected metrics
-        Processing and Storing Metrics
-        track the total number file
-        track the total number occurance of each status code
-    """
-    print("File size: {}".format(total_file_size))
-    for code in sorted(status_code_count.key()):
-        if status_code_count[code] > 0:
-            print("{}: {}".format(code, status_code_count[code]))
 
 def handle_interrupt(signal, frame):
     """Handle keyboard interrupt signal"""
-    print_metrics()
+    print_metrics(status_code_count, total_file_size)
     sys.exit(0)
 
+
 signal.signal(signal.SIGINT, handle_interrupt)
-for line in sys.stdin:
-    try:
-        parts =line.split()
-        if len(parts) < 9:
+try:
+    for line in sys.stdin:
+        parts = line.split()
+        parts = parts[::-1]
+
+        if len(parts) < 2:
             continue
-        file_size = int(parts[-1])
-        status_code = parts[-2]
+        file_size = int(parts[0])
+        status_code = parts[1]
 
         total_file_size += file_size
 
@@ -49,7 +61,7 @@ for line in sys.stdin:
         line_count += 1
 
         if line_count % 10 == 0:
-            print_metrics()
-    except Exception:
-        continue
-print_metrics()
+            print_metrics(status_code_count, total_file_size)
+except Exception as e:
+    pass
+print_metrics(status_code_count, total_file_size)
