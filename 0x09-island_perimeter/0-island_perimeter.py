21f6441dbc567340b8670f46bab8a49aea90caf6
Godwin <godwinchuks032@gmail.com>
2024-08-01 12:38:04 +0100

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -22,9 +22,9 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
                 if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
                 if j == len(grid[i]) - 1 or grid[i][j - 1] == 0:
                     perimeter += 1
